91ccd734e55fac34bd4ed923f5f4d0af59647408
erickwer <erickrodriguesmusic@hotmail.com>
2020-09-15 08:55:13 -0300

    Build Update

--- admin/src/environments/environment.prod.ts
+++ admin/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api-formalizacao-argumentos.thelogiclive.com/',
-  apiPlatUrl:'https://api.thelogiclive.com/'
+  apiPlatUrl:'https://api.thelogiclive.com/api/v1/'
 };
